e2dbe167c9fa735324bea7ddc43c61e12fd8a55e
maritana <kozyreva.91@gmail.com>
2024-12-19 19:47:55 +0300

    Вёрстка меню

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,14 @@
-/* в этот файл добавляет скрипты*/
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', () => {
+  if (navMain.classList.contains('main-nav--close')) {
+    navMain.classList.remove('main-nav--close');
+    console.log('open');
+  } else {
+    navMain.classList.add('main-nav--close');
+    console.log('close');
+  }
+});
