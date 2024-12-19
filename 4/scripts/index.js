25e7685a77679d9f84d7f07a8c17365f43888337
itmaritana <124173128+itmaritana@users.noreply.github.com>
2024-12-19 17:46:27 +0000

    Deploying to gh-pages from @ itmaritana/403849-cat-energy-2@45fc3f7c1df83ebd20644fadc852ca98eba2cd36 🚀

--- 4/scripts/index.js
+++
@@ -1,1 +0,0 @@
-var a=document.querySelector(".main-nav"),s=document.querySelector(".main-nav__toggle");a.classList.remove("main-nav--nojs");s.addEventListener("click",()=>{a.classList.contains("main-nav--close")?(a.classList.remove("main-nav--close"),s.classList.add("main-nav__toggle--close")):(a.classList.add("main-nav--close"),s.classList.remove("main-nav__toggle--close"))});
